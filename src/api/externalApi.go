c28266286e25bd1cbec08f864e000241f3b3e5f6
victorguidi <vsantosg4072@gmail.com>
2023-12-24 12:03:44 -0300

    integrated langchaingo, still need to fix the prompt

--- src/api/externalApi.go
+++ src/api/externalApi.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
-	"github.com/tmc/langchaingo/schema"
+	// "github.com/tmc/langchaingo/schema"
 	"github.com/victorguidi/LangJournaling/src/db"
 )
 
@@ -28,6 +28,7 @@ func (a *API) TranslateQuestions(language string, questions interface{}) (interf
 
 	var questionsToTranslate []string
 
+	// TODO: refactor this so questions do not repeat
 	for _, q := range questions.([]db.BaseQuestions) {
 		questionsToTranslate = append(questionsToTranslate, fmt.Sprintf(`
       question: %s
@@ -35,24 +36,37 @@ func (a *API) TranslateQuestions(language string, questions interface{}) (interf
       `, q.Question, q.Answer))
 	}
 
-	prompt := fmt.Sprintf(`YOU ARE A PROFESSIONAL LANGUAGE TEACHER. YOUR TASK IS TO TRANSLATE THE FOLLOWING QUESTIONS AND ANSWERS INTO %s. TRANSLATE THIS: %v`, language, strings.Join(questionsToTranslate, ""))
-	llm, err := ollama.NewChat(ollama.WithLLMOptions(ollama.WithModel("mistral")))
+	prompt := fmt.Sprintf(`
+	   YOU ARE A PROFESSIONAL TRANSLATOR.
+	   YOUR TASK IS TO TRANSLATE THE FOLLOWING QUESTIONS AND ANSWERS INTO %s LANGUAGE.
+
+    BEGIN TRANSLATION==========================================
+	   %v
+    END TRANSLATION============================================
+
+     `,
+		language, strings.Join(questionsToTranslate, ""))
+
+	llm, err := ollama.New(ollama.WithModel("mistral"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-	completion, err := llm.Call(ctx, []schema.ChatMessage{
-		schema.SystemChatMessage{Content: prompt},
-		schema.HumanChatMessage{Content: "Translate the questions please"},
-	}, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-		fmt.Print(string(chunk))
-		return nil
-	}),
+	completion, err := llm.Call(ctx, prompt,
+		llms.WithTemperature(0.8),
+		// llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		// 	// fmt.Print(string(chunk))
+		// 	return nil
+		// }),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = completion
+
+	final := strings.Split(completion, "\n")
+	for _, f := range final {
+		fmt.Println(f)
+	}
 
 	return nil, nil
 }
